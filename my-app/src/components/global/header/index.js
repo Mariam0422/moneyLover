dc81ec0c478c7d68988a8b384bae298c197fb55b
Mariam0422 <mariam79.yan@gmail.com>
2024-09-23 14:04:45 +0400

    add login component

--- my-app/src/components/global/header/index.js
+++ my-app/src/components/global/header/index.js
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router";
-
+import { Link } from "react-router-dom";
+import { Layout, Button } from "antd";
+import logo from "../../../img/logo.jpg";
+import "./index.css";
 const Header = () => {
-    return ( 
-        <div>
-            Header
-            <Outlet/>
-        </div>
-     );
-}
- 
+  return (
+    <Layout.Header>
+      <div className="mainHeader">
+        <img src={logo} alt="logo" style={{ width: "40px", height: "40px" }} />
+
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
+
+      </div>
+      <Outlet />
+    </Layout.Header>
+  );
+};
+
 export default Header;
--- my-app/src/pages/login/index.js
+++ my-app/src/pages/login/index.js
@@ -1,7 +1,27 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../services/firebase";
+import { Form, Input } from "antd";
+import AuthWrapper from "../../components/shared/AuthWrapper";
+import loginImg from "../../img/loginImg.jpg"
+import './index.css';
 const Login = () => {
+    const [form] = Form.useForm();
+    const handleLogin = async() => {
+      
+    }
   return (
-  <div>
-   logimnnnhmfghsrtythfgrt
+  <div className="loginMain">
+    <AuthWrapper coverImg={loginImg} maxWidth="500px">
+   <h2>Login</h2>
+   <Form form={form} onFinish={handleLogin}>
+    <Form.Item name="email" label="Email">
+        <Input/>
+    </Form.Item>
+    <Form.Item name="password" label="Password">
+        <Input.Password/>
+    </Form.Item>
+   </Form>
+   </AuthWrapper>
     </div>
     );
 };
