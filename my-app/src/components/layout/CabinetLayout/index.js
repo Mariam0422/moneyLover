d7f60dfbbb3fdce116352d4e1b6c1e08cd0b9735
Mariam0422 <mariam79.yan@gmail.com>
2024-10-02 00:35:35 +0400

    add store column

--- my-app/src/components/layout/CabinetLayout/index.js
+++ my-app/src/components/layout/CabinetLayout/index.js
@@ -1,16 +1,13 @@
-
-import Sidebar from '../../global/sideBar';
-import CabinetBoard from '../../../pages/cabinetBoard';
-import './index.css';
+import CabinetBoard from "../../../pages/cabinetBoard";
+import "./index.css";
 
 const CabinetLayout = () => {
-    return (
-        <div className='cabinet_layout_container'>
-      <Sidebar/>     
-         <main className="main_container">
-         <CabinetBoard/>
-         </main>
-        </div>
-    )
-}
+  return (
+    <div className="cabinet_layout_container">
+      <main className="main_container">
+        <CabinetBoard />
+      </main>
+    </div>
+  );
+};
 export default CabinetLayout;
--- my-app/src/pages/cabinetBoard/index.js
+++ my-app/src/pages/cabinetBoard/index.js
@@ -4,24 +4,26 @@ import "./index.css";
 
 const CabinetBoard = () => {
   return (
-    <div>
-      <div className="main">
-        <div className="card">
-          <div className="visa">VISA</div>
-          <div className="text">0000 0000 0000 0000</div>
-          <div className="footer">
-            <div className="cardHolder">Ruben Rubinyan</div>
-            <div className="term">05/28</div>
-          </div>
-        </div>
-        <div className="btnClass">
-          <Button className="plusBtn">
-            <span style={{ marginTop: "-7px" }}>+</span>
-          </Button>
-          <p className="btnText">Add new expenses</p>
+    <div className="main">
+      <div >
+      <div className="card">
+        <div className="visa">VISA</div>
+        <div className="text">0000 0000 0000 0000</div>
+        <div className="footer">
+          <div className="cardHolder">Ruben Rubinyan</div>
+          <div className="term">05/28</div>
         </div>
       </div>
-    <IconComponents/>
+      <div className="btnClass">
+        <Button className="plusBtn">
+          <span style={{ marginTop: "-7px" }}>+</span>
+        </Button>
+        <p className="btnText">Add new expenses</p>
+      </div>
+
+      <IconComponents />
+      </div>
+      <div className="story"></div>
     </div>
   );
 };
